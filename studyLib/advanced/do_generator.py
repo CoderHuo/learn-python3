f072bcc79c3ebeca4436e7a1ffb2b5e6ef1e1bc8
CoderHuo <huo_shua6061@hotmail.com>
2017-02-21 15:08:30 +0800

    generator

--- studyLib/advanced/do_generator.py
+++ studyLib/advanced/do_generator.py
@@ -3,16 +3,35 @@
 __author__ = 'Mr.Huo'
 
 
+# yield 可以简单理解为一个断点，返回一个值，下次从yield后执行，当没有yield可执行，即出现StopIteration异常
+def odd():
+    print('step 1')
+    yield 1
+    print('step 2')
+    yield 2
+    print('yield end')
 
 
 def main():
-    #generator：一边循环一边计算
-    #创建generator，根据列表生成式创建
-    L=(x*x for x in range(10))
+    # generator：一边循环一边计算
+    # 创建generator，根据列表生成式创建
+    L = (x * x for x in range(10))
     print(L)
     print(L.__next__())
+    for x in L:
+        print(x)
+    # 通过函数中带yield创建一个generator
+    gen = odd()
+    print('generator next 1')
+    print(gen.__next__())
+    print('generator next 2')
+    print(gen.__next__())
+    try:
+        print(gen.__next__())
+    except Exception as err:
+        print(err)
 
-    D=()
+    print(list(odd()))
 
 
 if __name__ == '__main__':
--- studyLib/funnyLib/funLib.py
+++ studyLib/funnyLib/funLib.py
@@ -10,45 +10,31 @@ def Hanoi_Move(n, a='a', b='b', c='c'):
     Hanoi_Move(n - 1, b, a, c)
 
 
-def fibonacci_big(end=10):
+def fibonacci_max(max=10):
     '''斐波那契数列生成器，按照最大值生成'''
-    a = b = 1
-    if a < end:
-        yield a
-    if b < end:
+    a = 0
+    b = 1
+    while b < max:
         yield b
-    while True:
         a, b = b, a + b
-        if (b < end):
-            yield b
-        else:
-            break
 
 
 def fibonacci_cnt(cnt=10):
     '''斐波那契数列生成器，按照个数生成'''
     cn = 0
-    a = b = 1
-    if cn < cnt:
-        cn += 1
-        yield a
-    if cn < cnt:
-        cn += 1
+    a = 0
+    b = 1
+    while cn<cnt:
         yield b
-    while True:
         a, b = b, a + b
-        if cn < cnt:
-            cn += 1
-            yield b
-        else:
-            break
+        cn += 1
 
 
 def main():
     Hanoi_Move(10)
-    print(list(fibonacci_big(10000)))
+    print(list(fibonacci_max(10000)))
     print(list(fibonacci_cnt(2)))
-    print(list(fibonacci_cnt(10)))
+    print(list(fibonacci_cnt(20)))
 
 
 if __name__ == '__main__':
