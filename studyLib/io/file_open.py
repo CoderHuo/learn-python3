363d38a653a9aeee1fd3476f2a1f2e435caf6239
CoderHuo <huo_shua6061@hotmail.com>
2017-02-23 18:02:04 +0800

    io

--- studyLib/io/file_open.py
+++ studyLib/io/file_open.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 __author__ = 'Mr.Huo'
 
 
+# 读写文件前，我们先必须了解一下，在磁盘上读写文件的功能都是由操作系统提供的，
+# 现代操作系统不允许普通的程序直接操作磁盘，所以，读写文件就是请求操作系统打开一个文件对象（通常称为文件描述符），
+# 然后，通过操作系统提供的接口从这个文件对象中读取数据（读文件），或者把数据写入这个文件对象（写文件）。
+
+
 def main():
-    pass
+    # file 打开失败会有异常，用try ... finally 始终关闭文件
+    try:
+        file_op = open('file_open.py', encoding='UTF-8')
+        print(file_op.read())
+    except Exception as err:
+        print(err)
+    finally:
+        file_op.close()
+
+    # 还有一种方式 with ...  as,自动调用close()方法
+    # read() 读取全部内容，读小文件可以，读取大文件使用read(size)
+    # readline() 读取一行内容
+    # readlines()一次读取所有内容并按行返回list
+    with open('file_open.py', encoding='UTF-8') as file_op:
+        print(file_op.read())
+
+    with open('file_open.py', encoding='UTF-8') as file_op:
+        for line in file_op.readlines():
+            print(line, end="")
+
 
 
 if __name__ == '__main__':
--- studyLib/unitTest/mydict_test.py
+++ studyLib/unitTest/mydict_test.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import unittest
+import sys
+
 __author__ = 'Mr.Huo'
 
+print(sys.path)
+from studyLib.unitTest.mydict import MyDict
 
-def main():
-    pass
+
+class TestMyDict(unittest.TestCase):
+    def test_init(self):
+        d = MyDict(a=1, b='test', test=2)
+        self.assertEqual(d.a, 1)
+        self.assertEqual(d.b, 'test')
+        self.assertEqual(d.test, 2)
+        self.assertTrue(isinstance(d, dict))
+
+    def test_key(self):
+        d = MyDict()
+        d['key'] = 'value'
+        self.assertTrue('key' in d)
+        self.assertEqual(d.key, 'value')
+
+    def test_attr(self):
+        d = MyDict()
+        d.key = 'value'
+        self.assertTrue('key' in d)
+        self.assertEqual(d['key'], 'value')
 
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
