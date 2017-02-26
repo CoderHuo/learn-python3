f73d8f2c66c85be8a515c2f9afa341f6edc8d9e0
CoderHuo <huo_shua6061@hotmail.com>
2017-02-26 17:45:49 +0800

    format{}

--- studyLib/advanced/formatPrint.py
+++ studyLib/advanced/formatPrint.py
@@ -11,6 +11,11 @@ def main():
     %d 整数
     %? 占位符，%0? 只能用0当占位符
     %f 浮点数，%.3f指定保留小数点位数
+    %e 科学计数法
+    %x 十六进制整数
+    %o 八进制整数
+    %g 十进制或科学计数法表示的浮点数
+    %% 文本值%本身
 '''
     day = 20170219
     pi = 3.1415926
@@ -30,6 +35,22 @@ def main():
     # 科学计数 .3e 小数后位数
     print('科学计数    -> |%10s|' % format(0.0000000023, '.2e'))
 
+    # {} format
+    n = 42
+    f = 7.03
+    s = 'string cheese'
+    #{}里面的数字为format后变量的序号
+    print('{2} {1} {0}'.format(n,f,s))
+
+    print('{0:d} {1:f} {2:s}'.format(n,f,s))
+    d = dict(n = 42,f = 7.03,s='string cheese')
+    #{0}代表整个字典
+    print('{0[n]} {0[f]} {0[s]} {1}'.format(d,'hello'))
+    #域宽 >右对齐  <左对齐 ^居中,字符串默认对齐方式为左，尽量不要使用默认，明确指明对齐方式
+    print('{0:10s}'.format('hello'))
+    print('{0[n]:15}|\n{0[f]:15}|\n{0[s]:15s}|\n{1:^15s}|'.format(d,'hello'))
+    p_str= '{0[n]:15}|\n{0[f]:<15}|\n{0[s]:>15s}|\n{1:^15s}|'.format(d,'hello')
+    print(p_str)
 
 if __name__ == '__main__':
     main()
