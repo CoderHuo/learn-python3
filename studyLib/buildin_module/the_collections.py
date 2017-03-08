f20802505fe6a3a85ee559e5d22a5e988e6b661e
CoderHuo <huo_shua6061@hotmail.com>
2017-03-08 15:48:17 +0800

    git

--- studyLib/buildin_module/the_collections.py
+++ studyLib/buildin_module/the_collections.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+from collections import namedtuple
+
 __author__ = 'Mr.Huo'
 
 
 def main():
-    pass
+    Point = namedtuple('Point',['x','y'])
+    p1 =Point(1,2)
+    print(p1.x,p1.y)
 
 
 if __name__ == '__main__':
