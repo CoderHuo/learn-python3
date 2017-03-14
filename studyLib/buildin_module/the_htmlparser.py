98e9758fdbbc322445273cc4d38623117f3b86cf
CoderHuo <huo_shua6061@hotmail.com>
2017-03-14 18:03:37 +0800

    html

--- studyLib/buildin_module/the_htmlparser.py
+++ studyLib/buildin_module/the_htmlparser.py
@@ -11,25 +11,27 @@ class MyHtmlParser(HTMLParser):
     def handle_startendtag(self, tag, attrs):
         self.handle_starttag(tag, attrs)
         self.handle_endtag(tag)
-        print('startendtag <%s>' % tag)
+        print('startendtag %s' % tag)
 
     # Overridable -- handle start tag
     def handle_starttag(self, tag, attrs):
-        print('starttag <%s>' % tag)
+        print('starttag %s' % tag)
         print('startattrs %s' % attrs)
         pass
 
     # Overridable -- handle end tag
     def handle_endtag(self, tag):
-        print('endtag </%s>' % tag)
+        print('endtag %s' % tag)
         pass
 
     # Overridable -- handle character reference
     def handle_charref(self, name):
+        print('charref %s' % name)
         pass
 
     # Overridable -- handle entity reference
     def handle_entityref(self, name):
+        print('entityref %s' % name)
         pass
 
     # Overridable -- handle data
@@ -42,21 +44,25 @@ class MyHtmlParser(HTMLParser):
         print('comment=%s' % data)
         pass
 
-    # Overridable -- handle declaration
+    # Overridable -- handle
     def handle_decl(self, decl):
+        print('decl=%s' % decl)
         pass
 
     # Overridable -- handle processing instruction
     def handle_pi(self, data):
+        print('pi=%s' % data)
         pass
 
 
-htmldata = '''<html html=1>
-<head head=1 head1=2>huo shao hua</head>
+htmldata = '''<html>
+<head>huo shao hua</head>
 <body>
 <!-- test html parser -->
-    <p>Some <a href=\"#\">html</a> HTML&nbsp;tutorial...<br>END</p>
-</body></html>
+<p>Some<a href=\"#\">html</a> HTML&nbsp;tutorial...<br>END
+</p>
+</body>
+</html>
 '''
 
 
