c13ef2702f86bea9bb1a752876566604899b67ba
CoderHuo <huo_shua6061@hotmail.com>
2017-03-19 19:16:26 +0800

    alien_version_1.0.1

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -6,19 +6,27 @@ __author__ = 'Mr.Huo'
 import pygame
 import sys
 
+
 def run_game():
-    #初始化游戏并创建一个屏幕对象
+    # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invision")
 
-    #开始游戏的主循环
+    #设置背景颜色
+    bg_color = (230,230,230)
+
+    # 开始游戏的主循环
     while True:
-        #监视键盘和鼠标事件
+        # 监视键盘和鼠标事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        screen.fill(bg_color)
+        # 让最近绘制的屏幕可见
         pygame.display.flip()
+
 
 def main():
     run_game()
