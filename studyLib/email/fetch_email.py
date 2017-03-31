7b1694703c3dc93b9b167587f824978f02ff9553
CoderHuo <huo_shua6061@hotmail.com>
2017-03-31 16:48:48 +0800

    parse email msg

--- studyLib/email/fetch_email.py
+++ studyLib/email/fetch_email.py
@@ -3,6 +3,8 @@
 
 import poplib
 from email.parser import Parser
+from email.utils import parseaddr
+from email.header import decode_header
 
 __author__ = 'Mr.Huo'
 
@@ -10,6 +12,56 @@ __author__ = 'Mr.Huo'
 第一步：用poplib 把邮件的原始文本下载到本地
 第二步：用email解析原始文本，还原邮件对象
 '''
+
+
+def print_info(msg, index=0):
+    """分析邮件"""
+    if index == 0:
+        for header in ['From', 'To', 'Subject']:
+            value = msg.get(header, '')
+            if value:
+                if header == 'Subject':
+                    value = decode_str(value)
+                #elif header == 'To':
+                #    print('To==========BEGIN',value,'To==========END')
+                else:
+                    hdr, addr = parseaddr(value)
+                    name = decode_str(hdr)
+                    value = u'%s <%s>' % (name, addr)
+            print(('%s%s: %s') % (' ' * index, header, value))
+    if (msg.is_multipart()):
+        parts = msg.get_payload()
+        for n, part in enumerate(parts):
+            print('%s part %s' % (' ' * index, n))
+            print('%s------------------------' % (' ' * index))
+            print_info(part, index + 1)
+    else:
+        content_type = msg.get_content_type()
+        if content_type in ('text/plain', 'text/html'):
+            content = msg.get_payload(decode=True)
+            charset = guess_charset(msg)
+            if charset:
+                content = content.decode(charset)
+            print('%sText: %s' % (' ' * index, content + '...'))
+        else:
+            print('%sAttachment: %s' % (' ' * index, content_type))
+
+
+def guess_charset(msg):
+    charset = msg.get_charset()
+    if charset is None:
+        content_type = msg.get('Content-Type', '').lower()
+        pos = content_type.find('charset=')
+        if pos > 0:
+            charset = content_type[pos + 8:].strip()
+    return charset
+
+
+def decode_str(s):
+    value, charset = decode_header(s)[0]
+    if charset:
+        value = value.decode(charset)
+    return value
 
 
 def main():
@@ -39,10 +91,12 @@ def main():
     resp, lines, octets = email_server.retr(len(mails))
     msg_content = b'\r\n'.join(lines).decode()
     msg = Parser().parsestr(msg_content)
-    print(msg)
+    #print(msg)
+    print_info(msg)
     # 可以根据邮件索引直接从服务器中删除邮件
     # email_server.dele(len(mails))
     # 关闭连接
+
     email_server.quit()
 
 
