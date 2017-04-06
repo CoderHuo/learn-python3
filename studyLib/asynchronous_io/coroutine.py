cfaf5f4d743014798d61e6fa5e5e76074fc29759
CoderHuo <huo_shua6061@hotmail.com>
2017-04-06 11:08:28 +0800

    yield

--- studyLib/asynchronous_io/coroutine.py
+++ studyLib/asynchronous_io/coroutine.py
@@ -4,21 +4,17 @@
 __author__ = 'Mr.Huo'
 
 def consumer():
-    print('3333')
     r = ''
     while True:
-        print('4444')
         n = yield r
-        print('5555')
         if not n:
             return
         print('[CONSUMER] Consuming %s...' % n)
         r = '200 OK'
 
 def produce(c):
-    print('1111')
-    c.send(None)
-    print('2222')
+    #c.send(None)
+    c.__next__()
     n = 0
     while n < 5:
         n = n + 1
@@ -35,3 +31,23 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+    def countdown(n):
+        print("Counting down from", n)
+        while n >= 0:
+            newvalue = (yield n)
+            # If a new value got sent in, reset n with it
+            if newvalue is not None:
+                print(111)
+                n = newvalue
+            else:
+                n -= 1
+
+
+    # The holy grail countdown
+    c = countdown(5)
+    for x in c:
+        print(x)
+        if x == 5:
+            print(c.send(None))
