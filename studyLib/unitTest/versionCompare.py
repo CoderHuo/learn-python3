a0ce636103be38c72412af0088171075ef5951be
CoderHuo <huo_shua6061@hotmail.com>
2017-04-07 18:01:20 +0800

    ver compare

--- studyLib/unitTest/versionCompare.py
+++ studyLib/unitTest/versionCompare.py
@@ -9,37 +9,44 @@ __author__ = 'Mr.Huo'
 def version_compare(v1, v2):
     v1 = _parse_version(v1)
     v2 = _parse_version(v2)
-    try:
-        return (v1 > v2) - (v1 < v2)
-    except:
-        return False
+    minlength = 0
+    print(len(v1),len(v2))
+    if len(v1) < len(v2):
+        result = compare(v1, v2, len(v1))
 
 
-def _parse_version(ver,separator='.',ignorecase=True):
+def compare(v1, v2, length):
+    for i in range(length):
+        if isinstance(v1[i],list):
+            pass
+    pass
+
+
+def _parse_version(ver, separator='.', ignorecase=True):
     if ver == '' or ver == None:
         raise RuntimeError("Version is null")
     if ignorecase:
-        ver =ver.lower()
+        ver = ver.lower()
     pattern = re.compile(r'\d+|\D+')
     ver_list = [int(x) if x.isdigit() else [int(y) if y.isdigit() else y for y in pattern.findall(x)] for x in
                 ver.split(separator)]
+    print(ver_list)
+    # 去除末尾多余0及空标识
+    while ver_list:
+        if ver_list[-1] == [] or ver_list[-1] == 0:
+            ver_list.pop()
+        else:
+            break
+    print(ver_list)
     return ver_list
 
 
 def main():
     v1 = '111b-2-@Aw**--.2..1'
-    v2 = '111b-2-@Aw**--.2..1'
-    # res = version_compare(v1, v2)
+    v2 = '111b-2-@Aw**--.2..1..0.01.00..'
     version_compare(v1, v2)
-    pattern = re.compile(r'\d+|\D+')
-    match = pattern.findall(v2)
-
-    print(match)
-    print([int(x) if x.isdigit() else x for x in match])
-    print(_parse_version(v2))
     pass
 
 
 if __name__ == '__main__':
     main()
-    print( ''<'1')
