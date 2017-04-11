10b98780e80bc0391099ac50c83e1c678fee6147
CoderHuo <huo_shua6061@hotmail.com>
2017-04-11 15:16:21 +0800

    test ver cmp

--- studyLib/unitTest/versionCompare_test.py
+++ studyLib/unitTest/versionCompare_test.py
@@ -2,57 +2,49 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from .versionCompare import version_compare
+from versionCompare import version_compare
 
 __author__ = 'Mr.Huo'
 
 
-class TestVerCom(unittest.TestCase):
-
-    def setUp(self):
-        self.v1 = ''
-        self.v2 = '10'
-        self.v3 = '11'
-        self.v4 = 'ab'
-        self.v5 = 'ac'
-        #self.v6 = '12ab'
-        #self.v7 = '1ab'
-        self.v7 = '12ab1'
-        self.v5 = '.10'
-        self.v6 = '.ab'
-        self.v7 = '.ab12'
-        self.v8 = '12ab.'
-        self.v9 = '1abc.0'
-        self.v10 = '2ab.A'
-        self.v11 = '11a2.1a'
-
-    def test_two_version_length_equal_v1_v2_element_have_same_length_list(self):
+class TestVerCmp(unittest.TestCase):
+    def test_version_length_equal_element_have_same_length_list(self):
         """测试长度相等"""
-        self.assertEqual(version_compare(self.v1, self.v1), 0)
-        self.assertEqual(version_compare(self.v1, self.v2), -1)
-        self.assertEqual(version_compare(self.v2, self.v1), 1)
-        self.assertEqual(version_compare(self.v3, self.v4), -1)
-        self.assertEqual(version_compare(self.v5, self.v5), 0)
-        self.assertEqual(version_compare(self.v5, self.v6), -1)
+        v1 = '10.1a'
+        v2 = '10.2a'
+        v3 = '10.2b'
+        v4 = '10.b1'
+        v5 = '.1'
+        v6 = '.a'
+        v7 = 'a.2'
+        self.assertEqual(version_compare(v1, v1), 0)
+        self.assertEqual(version_compare(v1, v2), -1)
+        self.assertEqual(version_compare(v2, v1), 1)
+        self.assertEqual(version_compare(v3, v4), -1)
+        self.assertEqual(version_compare(v5, v5), 0)
+        self.assertEqual(version_compare(v5, v6), -1)
+        self.assertEqual(version_compare(v5, v7), -1)
+        self.assertEqual(version_compare(v7, v5), 1)
 
-    def test_two_version_length_equal_v1_v2_element_have_unequal_length_list(self):
-        v1 = '1.2b2c'
-        v2 = '1.2B1'
-        v3 = '1.'
+    def test_version_length_equal_element_have_unequal_length_list(self):
+        v1 = '10.11a'
+        v2 = '10.1a20'
+        v3 = '10.0b'
+        v4 = '10.0b1'
         self.assertEqual(version_compare(v1, v2), 1)
         self.assertEqual(version_compare(v2, v1), -1)
-        self.assertEqual(version_compare(v1, v3), 1)
-        self.assertEqual(version_compare(v3, v1), -1)
+        self.assertEqual(version_compare(v3, v4), -1)
+        self.assertEqual(version_compare(v4, v3), 1)
 
     def test_length_unequal(self):
         """测试长度不相等"""
-        v1 = '1.2.3'
+        v1 = '1.2'
         v2 = '1'
         self.assertEqual(version_compare(v1, v2), 1)
         self.assertEqual(version_compare(v2, v1), -1)
 
     def test_empty(self):
-        """测试长度为空"""
+        """测试空"""
         v1 = ''
         v2 = '1'
 
@@ -62,7 +54,6 @@ class TestVerCom(unittest.TestCase):
             version_compare(v2, v1)
         with self.assertRaises(RuntimeError):
             version_compare(v1, v1)
-
 
 if __name__ == '__main__':
     unittest.main()
