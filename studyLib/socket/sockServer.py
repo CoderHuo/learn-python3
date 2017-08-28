d3a158caf356aeba97a33e4732079bdd6d8ee62e
shaohua huo <huo_shua6061@hotmail.com>
2017-08-28 17:50:33 +0800

    add comment

--- studyLib/socket/sockServer.py
+++ studyLib/socket/sockServer.py
@@ -13,17 +13,19 @@ TcpSerAddr = (LOCALIP, TCP_PORT_S)
 class MyRequestHandler(SRH):
     def handle(self):
         print('...connect from:', self.client_address)
-        while True:
+        flag = True
+        while flag:
             data = self.rfile.readline()
             print(type(data))
             if not data or b'ByeBye' in data:
-                self.wfile.write(('[%s] %s' % (ctime(), b'ByeBye'.decode("UTF-8"))).encode("UTF-8"))
-                break
+                flag = False
             print(data)
             self.wfile.write(('[%s] %s' % (ctime(), data.decode("UTF-8"))).encode("UTF-8"))
 
 
 def main():
+    # TCPServer，IO多路复用，但是StreamRequestHandler还是同步阻塞IO
+    # 同步阻塞IO、同步非阻塞IO、IO多路复用、异步IO
     tcpServ = TCP(TcpSerAddr, MyRequestHandler)
     print('waiting for connection...')
     tcpServ.serve_forever()
