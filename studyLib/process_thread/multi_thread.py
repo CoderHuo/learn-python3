8727b72cb451051f1c524b69498a307ce712de60
shaohua huo <huo_shua6061@hotmail.com>
2017-09-07 11:17:53 +0800

    multi thread

--- studyLib/process_thread/multi_thread.py
+++ studyLib/process_thread/multi_thread.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from threading import Thread
+from threading import Thread, current_thread, Lock
 import multiprocessing
 import time
 
@@ -12,7 +12,22 @@ def counter():
     i = 0
     for _ in range(100000000):
         i += 1
+    print(i)
     return True
+
+
+num = 0
+
+lock = Lock()
+
+
+def add():
+    global num
+    for i in range(1000000):
+        lock.acquire()
+        num += 1
+        lock.release()
+    print(num, current_thread().name)
 
 
 def main():
@@ -31,6 +46,19 @@ def main():
     print("Total time: {}".format(end_time - start_time))
 
     # 多进程
+    process_array = list()
+    process_start = time.time()
+    for pid in range(2):
+        proc = multiprocessing.Process(target=counter)
+        proc.start()
+        process_array.append(proc)
+    for i in range(len(process_array)):
+        process_array[i].join()
+
+    process_end = time.time()
+    print("Total time: {}".format(process_end - process_start))
+
+    # 多进程 Pool
     mp = multiprocessing.Pool()
     mp_start = time.time()
     for i in range(2):
@@ -39,6 +67,17 @@ def main():
     mp.join()
     mp_end = time.time()
     print("Total time: {}".format(mp_end - mp_start))
+
+    add_array = {}
+    for tid in range(10):
+        add_thread = Thread(target=add)
+        add_thread.start()
+        add_array[tid] = add_thread
+
+    for tid in range(len(add_array)):
+        add_array[tid].join()
+
+    print(num)
 
 
 if __name__ == '__main__':
