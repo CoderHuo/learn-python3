a9a8d461474ede8f1209839d6990be8086685428
shaohua huo <huo_shua6061@hotmail.com>
2017-10-24 16:25:27 +0800

    parse job data(location category)

--- spy/spy/pipelines.py
+++ spy/spy/pipelines.py
@@ -10,14 +10,26 @@ import json, codecs
 class SpyPipeline(object):
     def __init__(self):
         self.file = open('example.json', 'w', encoding='utf-8')
-        self.jobData = {"job":[]}
-        #self.job
+        self.jobData = {"job": []}
+        self.job_location = {}
+        self.job_category = {}
 
     def process_item(self, item, spider):
         self.jobData["job"].append(dict(item))
+        self.collect_job(self.job_location, 'workLocation', item)
+        self.collect_job(self.job_category, 'category', item)
         return item
 
     def close_spider(self, spider):
+        self.jobData['location'] = self.job_location
+        self.jobData['category'] = self.job_category
         line = json.dumps(dict(self.jobData), ensure_ascii=False)
         self.file.write(line)
         self.file.close()
+
+    def collect_job(self, job_dict, key, item):
+        if item[key] in job_dict.keys():
+            job_dict[item[key]] += int(item['recruitNumber'])
+        else:
+            # 初始化
+            job_dict[item[key]] = int(item['recruitNumber'])
